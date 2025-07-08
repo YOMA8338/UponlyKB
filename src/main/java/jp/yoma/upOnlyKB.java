4ace3c6a725ca6163a1e5f7104d727cdeef5f9c0
YOMA8338 <lengfudaoguan@gmail.com>
2025-07-09 08:49:00 +0900

    コンフィグ機能の追加
    コンフィグで変更できるようにした。
    api-versionを1.13に変更した。
    プラグインの説明を日本語から英語に変更した。

--- src/main/java/jp/yoma/upOnlyKB.java
+++ src/main/java/jp/yoma/upOnlyKB.java
@@ -1,4 +1,9 @@
 package jp.yoma;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,17 +15,33 @@ import org.bukkit.util.Vector;
 
 public final class upOnlyKB extends JavaPlugin implements Listener {
 
+    private double horizontalMultiplier;
+    private double verticalBoost;
+
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        loadConfigValues();
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("uponlykb").setExecutor(this);
+        getLogger().info("UpOnlyKB Loaded.");
+
 
     }
+    private void loadConfigValues() {
+        reloadConfig();
+        FileConfiguration config = getConfig();
+        horizontalMultiplier = config.getDouble("knockback.horizontalMultiplier");
+        verticalBoost = config.getDouble("knockback.verticalBoost");
+    }
 
-    @Override
+
+    /*@Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
+    }*/
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
             // ダメージを受けたエンティティを取得
@@ -35,7 +56,7 @@ public final class upOnlyKB extends JavaPlugin implements Listener {
             if (player.getNoDamageTicks() <= 10) {
                 // Motion[1]を上向きに設定
                 if (!event.isCancelled()){
-                    player.setVelocity(new Vector(player.getVelocity().getX()*0.18, 0.353, player.getVelocity().getZ()*0.18));
+                    player.setVelocity(new Vector(player.getVelocity().getX()*horizontalMultiplier, verticalBoost, player.getVelocity().getZ()*horizontalMultiplier));
                 }
             }
         }
@@ -78,5 +99,16 @@ public final class upOnlyKB extends JavaPlugin implements Listener {
 
 
     }*/
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("uponlykb") && args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            loadConfigValues();
+            sender.sendMessage(ChatColor.GREEN + "UponlyKB's config reloaded.");
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "Usage: /uponlykb reload");
+        return false;
+    }
+
 }
 
