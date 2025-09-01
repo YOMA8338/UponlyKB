2ecd26374fd34f56fdfa2a7174ed528ab14ae1b6
YOMA8338 <lengfudaoguan@gmail.com>
2025-09-01 20:55:22 +0900

    コンフィグ更新確認機能と有効無効の切り替えの追加だけになった
    元々は、水平のノックバックの量を少なくしたりできるようにしたかった。
    なぜかできなかった。
    結果、コンフィグバージョン確認機能、有効無効切り替えの追加、無駄な改行の削除などだけになった。
    旧バージョンのコンフィグはconfig_old.ymlに名前変更した後、新しいコンフィグを再生成するようになっています。
    多分動きます。

--- src/main/java/jp/yoma/upOnlyKB.java
+++ src/main/java/jp/yoma/upOnlyKB.java
@@ -12,12 +12,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.util.Vector;
+import java.io.File;
 
 public final class upOnlyKB extends JavaPlugin implements Listener {
 
+    //values
     private double horizontalMultiplier;
     private double verticalBoost;
-
+    private boolean ckbactive; //Custom KnockBack Active flag
+    private int configversion;
+    private int requiredconfigversion = 1;
 
     @Override
     public void onEnable() {
@@ -27,42 +31,52 @@ public final class upOnlyKB extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
         getCommand("uponlykb").setExecutor(this);
         getLogger().info("UpOnlyKB Loaded.");
-
-
     }
     private void loadConfigValues() {
         reloadConfig();
         FileConfiguration config = getConfig();
-        horizontalMultiplier = config.getDouble("knockback.horizontalMultiplier");
-        verticalBoost = config.getDouble("knockback.verticalBoost");
-    }
-
-
-    /*@Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }*/
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-            // ダメージを受けたエンティティを取得
-        Entity entity = event.getEntity();
-        //Entity attacker = event.getDamager();//今後使う可能性を考えて入れたままにしておく
-
-        // エンティティがプレイヤーである場合の処理
-        if (entity instanceof Player) {
-            Player player = (Player) entity;
-
-                
-            if (player.getNoDamageTicks() <= 10) {
-                // Motion[1]を上向きに設定
-                if (!event.isCancelled()){
-                    player.setVelocity(new Vector(player.getVelocity().getX()*horizontalMultiplier, verticalBoost, player.getVelocity().getZ()*horizontalMultiplier));
+        ckbactive = config.getBoolean("knockback.active",true);
+        horizontalMultiplier = config.getDouble("knockback.horizontalkb",1.0);
+        verticalBoost = config.getDouble("knockback.verticalkb",0.35);
+        configversion = config.getInt("config-version",0);
+        if (configversion < requiredconfigversion){
+            File configFile = new File(getDataFolder(), "config.yml");
+            File oldConfigFile = new File(getDataFolder(), "config_old.yml");
+            if (configFile.exists()) {
+                if (oldConfigFile.exists()) {
+                    oldConfigFile.delete();
+                }
+                boolean renamed = configFile.renameTo(oldConfigFile);
+                if (renamed) {saveDefaultConfig();}
+                if (renamed){
+                    getLogger().info("Renamed the old config file to config_old.yml and regenerated the config file.");
+                    loadConfigValues();
+                }else{
+                    getLogger().warning("Failed to backup the old config file!");
                 }
             }
         }
     }
-    
-    
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        //Get damaged entity
+        if (!ckbactive){return;}
+        Entity entity = event.getEntity();
+        //Entity attacker = event.getDamager();//ダメージを与えたエンティティを取得
+        //When Entity is Player, apply upward knockback
+        if (entity instanceof Player) {
+            Player player = (Player) entity;
+            if (player.getNoDamageTicks() <= 10) {
+                if (!event.isCancelled()){
+                    final Vector playerVelocity = player.getVelocity();
+                    player.setVelocity(new Vector((playerVelocity.getX()* horizontalMultiplier), verticalBoost, (playerVelocity.getZ()* horizontalMultiplier)));
+                    //player.setVelocity(new Vector(player.getVelocity().getX()*horizontalMultiplier, verticalBoost, player.getVelocity().getZ()*horizontalMultiplier));
+                }
+            }
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("uponlykb") && args.length == 1 && args[0].equalsIgnoreCase("reload")) {
@@ -70,8 +84,6 @@ public final class upOnlyKB extends JavaPlugin implements Listener {
                 sender.sendMessage(ChatColor.RED + "You have no permission!");
                 return true;
             }
-
-
             loadConfigValues();
             sender.sendMessage(ChatColor.GREEN + "UponlyKB's config reloaded.");
             return true;
@@ -79,6 +91,5 @@ public final class upOnlyKB extends JavaPlugin implements Listener {
         sender.sendMessage(ChatColor.RED + "Usage: /uponlykb reload");
         return false;
     }
-
 }
 
